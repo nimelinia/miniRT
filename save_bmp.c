7e17f1c9be8dc9660925e0265797564106de6063
Natalia Troyanovskaya <nataliatroyanovskaya@MacBook-Pro-Natalia-2.local>
2020-11-02 21:43:23 +0300

    pochinila kvadrat i save

---
+++ save_bmp.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   save_bmp.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scopycat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/29 16:52:29 by scopycat          #+#    #+#             */
+/*   Updated: 2020/10/31 11:24:13 by scopycat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+#include "minilibx_mms/mlx.h"
+#include "get_next_line.h"
+
+void	put_image_bmp(t_scene *scene)
+{
+	char			*name;
+	t_file_header	header;
+	t_bmp_info		bmpi;
+
+	name = scene->scene_name;
+	name[ft_strlen(name) - 2] = '\0';
+	name = ft_strjoin_gnl(name, "bmp");
+	header.file_size = scene->resol.x_size * scene->resol.y_size * 4 + 54;
+	header.signature = 0x4D42;
+	header.offset_to_pix = 54;
+	header.reserved = 0;
+	bmpi.headersize = 40;
+	bmpi.plane = 1;
+	bmpi.weight = scene->resol.x_size;
+	bmpi.hight = scene->resol.y_size;
+	bmpi.bpp = g_mlx.bpp;
+	bmpi.compress = 0;
+	bmpi.imagesize = scene->resol.x_size * scene->resol.y_size * 4;
+	bmpi.bpmx = 0;
+	bmpi.bpmy = 0;
+	bmpi.colour = 0;
+	bmpi.colout_imp = 0;
+	fill_bmp(bmpi, header, name, scene);
+}
+
+void	fill_bmp(t_bmp_info bmpi, t_file_header header, char *name, \
+		t_scene *scene)
+{
+	int	fd;
+	int i;
+
+	i = 0;
+	fd = open(name, O_WRONLY | O_TRUNC | O_CREAT, 0777);
+	i = write(fd, &header.signature, 2);
+	i += write(fd, &header.file_size, 4);
+	i += write(fd, &header.reserved, 4);
+	i += write(fd, &header.offset_to_pix, 4);
+	i += write(fd, &bmpi.headersize, 4);
+	i += write(fd, &bmpi.weight, 4);
+	i += write(fd, &bmpi.hight, 4);
+	i += write(fd, &bmpi.plane, 2);
+	i += write(fd, &bmpi.bpp, 2);
+	i += write(fd, &bmpi.compress, 4);
+	i += write(fd, &bmpi.imagesize, 4);
+	i += write(fd, &bmpi.bpmx, 4);
+	i += write(fd, &bmpi.bpmy, 4);
+	i += write(fd, &bmpi.colour, 4);
+	i += write(fd, &bmpi.colout_imp, 4);
+	if ((i != 54) || fill_pixels(fd, scene->resol.x_size, scene->resol.y_size)\
+		!= (bmpi.imagesize - 1))
+		exit(1);
+	close(fd);
+	exit(0);
+}
+
+int		fill_pixels(int fd, int x, int y)
+{
+	int				i;
+	unsigned char	*pic;
+
+	pic = (unsigned char*)g_mlx.addr;
+	i = 0;
+	while (y-- >= 0)
+		i += write(fd, &pic[g_mlx.length * y], x * 4);
+	return (i);
+}
--- square.c
+++ square.c
@@ -6,53 +6,58 @@
 /*   By: scopycat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 18:15:43 by scopycat          #+#    #+#             */
-/*   Updated: 2020/10/28 21:23:36 by scopycat         ###   ########.fr       */
+/*   Updated: 2020/11/02 21:42:17 by scopycat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 #include "minilibx_mms/mlx.h"
 
-void	draw_square(t_scene *scene, t_mlx *mlx, int x, int i) //void *mlx_ptr, void *win_ptr) //допилить свет и тени
+void		draw_square(t_scene *scene, t_mlx *mlx, int x, int i)
 {
-	int		y;
-	int		colour;
-	// double	t[2];
 	t_index		sq;
+
+	sq.i_f = 4;
+	sq.i_f_n = i;
+	sq.count = scene->square->count;
+	draw_square_util(scene, mlx, x, sq);
+}
+
+void		draw_square_util(t_scene *scene, t_mlx *mlx, int x, t_index sq)
+{
+	int			y;
+	int			i;
 	t_square	*tmp_sq;
 
-	y = 0;
-	sq.ind_fig = 4;
-	sq.ind_fig_n = i;
-	sq.count = scene->square->count;
 	tmp_sq = scene->square;
+	i = sq.i_f_n;
 	while (i--)
 		tmp_sq = tmp_sq->next;
 	while (x < scene->resol.x_size)
 	{
+		y = 0;
 		while (y < scene->resol.y_size)
 		{
-			if ((sq.t = check_square(scene, x, y, sq.ind_fig_n)) && sq.t <= scene->points[x][y])
+			if ((sq.t = check_square(scene, x, y, sq.i_f_n)) &&
+				sq.t <= scene->points[x][y])
 			{
 				sq.ind_l_n = 0;
-				colour = convert_colour(tmp_sq->colour, find_colour_2(scene, x, y, sq)); //find_colour_3(scene, x, y, t)); // тут пока без света - только рассеянный и без теней
-				my_mlx_pixel_put(mlx, x, y, colour);
-				// mlx_pixel_put(mlx_ptr, win_ptr, x, y, colour); //convert_colour(scene->sphere->colour, light));
+				my_mlx_pixel_put(mlx, x, y, convert_colour(tmp_sq->colour, \
+					find_colour_2(scene, x, y, sq)));
 				scene->points[x][y] = sq.t;
 			}
 			y++;
 		}
 		x += 4;
-		y = 0;
 	}
 }
 
-double	check_square(t_scene *scene, int x, int y, int i) // лажает на нормалях 1,0,0 0,1,0 и 0,0,1
+double		check_square(t_scene *scene, int x, int y, int i)
 {
 	t_xyzpoint	canvas;
 	double		close_t;
 	double		t;
-	t_xyzpoint	ray; // направление взгляда из камеры на точку на холсте
+	t_xyzpoint	ray;
 	t_square	*sq;
 
 	canvas = find_center_canvas(scene, x, y);
@@ -60,72 +65,69 @@ double	check_square(t_scene *scene, int x, int y, int i) // лажает на н
 	sq = scene->square;
 	while (i--)
 		sq = sq->next;
-   	sq->orient = normalize_orient(sq->center, sq->orient, scene->camera->center);
+	sq->orient = normalize_orient(sq->center, sq->orient, \
+		scene->camera->center);
 	ray = substruct_vector(canvas, scene->camera->center);
 	ray = normalize_vector(ray);
 	t = (scalar(sq->center, sq->orient) - scalar(sq->orient,  \
-		scene->camera->center)) / scalar(sq->orient, ray);
-	// t = length_to_pl_point(scene->camera->center, sq->orient, sq->center, ray);
-	if (t >= 1 && t < close_t && check_inside_square(scene->camera->center, sq, ray, t))
+		scene->camera->center)) / scalar(sq->orient, canvas);
+	if (t >= 1 && t < close_t && check_inside_square(scene->camera->center, \
+		sq, canvas, t))
 		close_t = t;
 	else
-		return (0);	
-	return (close_t);	
+		return (0);
+	return (close_t);
 }
 
-int		check_inside_square(t_xyzpoint cam, t_square *square, t_xyzpoint ray, double t) // разбить на подфункции - слишком много переменных
+int			check_inside_square(t_xyzpoint cam, t_square *square, \
+			t_xyzpoint ray, double t)
 {
-	t_xyzpoint	a;
-	t_xyzpoint	b;
-	t_xyzpoint	c;
-	t_xyzpoint	d;
-	double		e;
+	t_angles	sq;
 	double		diagonal;
 	t_xyzpoint	point;
 
-    // square->orient = normalize_vector(square->orient);
-    e = (scalar(square->center, square->orient)); // нахожу D из уравнения плоскости Ax + By + Cz + D = 0 (по факту это -D)
-	if (square->orient.x)
-	{
-		a.x =  e / square->orient.x;
-		a.y = 0;
-		a.z = 0;
-	}
-	else if (square->orient.y)
-	{
-		a.y =  e / square->orient.y;
-		a.x = 0;
-		a.z = 0;
-	}
-	else
-	{
-		a.z =  e / square->orient.z;
-		a.x = 0;
-		a.y = 0;
-	}
-	a = substruct_vector(square->center, a); // вектор из точки пересечения в центр квадрата
-	a = normalize_vector(a);
+	sq.a = find_first_angle(square);
 	diagonal = sqrt(2 * pow(square->lenght, 2));
-	c = find_point_coordinates(square->center, a, -(diagonal / 2));
-	a = find_point_coordinates(square->center, a, diagonal / 2);
-	b = substruct_vector(a, square->center); //(нахожу вектор из центра квадрата в первую вершину)
-	b = vector_mult(square->orient, b);
-	b = normalize_vector(b);
-	d = find_point_coordinates(square->center, b, -(diagonal / 2));
-	b = find_point_coordinates(square->center, b, diagonal / 2);
-	if (scalar (ray, square->orient) < 0)
+	sq.c = find_point_coordinates(square->center, sq.a, -(diagonal / 2));
+	sq.a = find_point_coordinates(square->center, sq.a, diagonal / 2);
+	sq.b = substruct_vector(sq.a, square->center);
+	sq.b = vector_mult(square->orient, sq.b);
+	sq.b = normalize_vector(sq.b);
+	sq.d = find_point_coordinates(square->center, sq.b, -(diagonal / 2));
+	sq.b = find_point_coordinates(square->center, sq.b, diagonal / 2);
+	if (scalar(ray, square->orient) < 0)
 		ray = mult_num_vect(ray, -1);
-    point = find_point_coordinates(cam, ray, t);
-	// if ((scalar(vector_mult(substruct_vector(b, a), substruct_vector(point, a)), vector_mult(substruct_vector(c, b), substruct_vector(point, b))) > 0 &&
-	//  	scalar(vector_mult(substruct_vector(c, b), substruct_vector(point, b)), vector_mult(substruct_vector(d, c), substruct_vector(point, c))) > 0 &&
-	// 	scalar(vector_mult(substruct_vector(d, c), substruct_vector(point, c)), vector_mult(substruct_vector(a, d), substruct_vector(point, d))) > 0 &&
-	// 	scalar(vector_mult(substruct_vector(a, d), substruct_vector(point, d)), vector_mult(substruct_vector(b, a), substruct_vector(point, a))) > 0))
-	// 	return (1);
-	if ((scalar(ray, vector_mult(substruct_vector(b, a), substruct_vector(point, a))) >= 0 &&
-	 	scalar(ray, vector_mult(substruct_vector(c, b), substruct_vector(point, b))) >= 0 &&
-		scalar(ray, vector_mult(substruct_vector(d, c), substruct_vector(point, c))) >= 0 &&
-		scalar(ray, vector_mult(substruct_vector(a, d), substruct_vector(point, d))) >= 0))
+	point = find_point_coordinates(cam, ray, t);
+	if ((scalar(ray, vector_mult(substruct_vector(sq.b, sq.a), \
+		substruct_vector(point, sq.a))) >= 0 && scalar(ray, \
+		vector_mult(substruct_vector(sq.c, sq.b), substruct_vector(point, \
+		sq.b))) >= 0 && scalar(ray, vector_mult(substruct_vector(sq.d, sq.c), \
+		substruct_vector(point, sq.c))) >= 0 && scalar(ray, vector_mult(\
+		substruct_vector(sq.a, sq.d), substruct_vector(point, sq.d))) >= 0))
 		return (1);
 	else
 		return (0);
+}
+
+t_xyzpoint	find_first_angle(t_square *square)
+{
+	t_xyzpoint	vector;
+	double		e;
+
+	e = (scalar(square->center, square->orient));
+	if (square->orient.x)
+		vector = (t_xyzpoint){e / square->orient.x, 0, 0};
+	else if (square->orient.y)
+		vector = (t_xyzpoint){0, e / square->orient.y, 0};
+	else
+		vector = (t_xyzpoint){0, 0, e / square->orient.z};
+	if (e == 0)
+	{
+		vector.x = square->orient.x == 0 ? 1 : square->orient.x;
+		vector.y = square->orient.y == 0 ? 1 : square->orient.y;
+		vector.z = square->orient.z == 0 ? 1 : square->orient.z;
+	}
+	vector = substruct_vector(square->center, vector);
+	vector = normalize_vector(vector);
+	return (vector);
 }
